b3f8212e911417ba1c4427749d6389c4a46d1d33
Richu Sony <sonyrichu4@gmail.com>
2024-04-07 23:09:59 +0530

    fix: jwt expiry

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,9 +2,10 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
 let complete = false;
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET,{expiresIn: '1h'});
     console.log("checking token",userId, " ::: ", token)
     res.cookie("jwt", token, {
+    maxAge:60 * 60 * 1000,
     httpOnly: true,
     sameSite: "none",
     secure: process.env.NODE_ENV !== "development",
