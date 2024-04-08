329e14a78614b69ad24e1115ddec34e956da1821
Richu Sony <sonyrichu4@gmail.com>
2024-04-08 17:37:50 +0530

    fix: user's friends chat issue

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -5,9 +5,30 @@ export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
 
-    const filteredUsers = await userModel.find({
-      _id: { $ne: loggedInUserId },
+    const userDetails = await userModel.findById(loggedInUserId)
+      .populate({
+        path: 'friends.friendId', // Populate the 'friendId' field of 'friends' array
+        model: 'users', // Reference model to populate from
+        select: 'fullname username email profileImage', // Specify which fields to include
+      })
+      .exec();
+
+    const uniqueUsersMap = new Map(); // Use a Map to track unique friendIds
+    userDetails.friends.forEach(friend => {
+      const { friendId, status } = friend;
+      uniqueUsersMap.set(friendId._id.toString(), {
+        _id: friendId._id,
+        fullname: friendId.fullname,
+        username: friendId.username,
+        email: friendId.email,
+        profileImage: friendId.profileImage,
+        status,
+      });
     });
+
+    const filteredUsers = [...uniqueUsersMap.values()];
+
+    console.log("filetered users :: ", filteredUsers);
     res.status(200).json(filteredUsers);
   } catch (error) {
     res
@@ -108,7 +129,9 @@ export const followUserRequest = async (req, res) => {
 export const getNotifications = async (req, res) => {
   try {
     const currentUserId = req.user._id;
-    const notifications = await notificationModel.find({ userId: currentUserId }).populate("senderId");
+    const notifications = await notificationModel
+      .find({ userId: currentUserId })
+      .populate("senderId");
 
     if (notifications.length > 0) {
       res.status(200).json(notifications);
@@ -122,41 +145,45 @@ export const getNotifications = async (req, res) => {
 };
 
 export const acceptFriendRequest = async (req, res) => {
-    const currentUserId = req.user._id;
-    const { id } = req.params;
+  const currentUserId = req.user._id;
+  const { id } = req.params;
 
-    try {
-        // Update the friend request status in the other user's document
-        const updatedUser = await userModel.findOneAndUpdate(
-            { _id: id, "friends.friendId": currentUserId },
-            { $set: { "friends.$.status": true } }, // Update the status to true for the specific friendId
-            { new: true } // Return the updated document
-        );
+  try {
+    // Update the friend request status in the other user's document
+    const updatedUser = await userModel.findOneAndUpdate(
+      { _id: id, "friends.friendId": currentUserId },
+      { $set: { "friends.$.status": true } }, // Update the status to true for the specific friendId
+      { new: true } // Return the updated document
+    );
 
-        // Add the corresponding relation to the current user's document
-        const updateCurrentUserRelation = await userModel.updateOne(
-            { _id: currentUserId },
-            {
-                $push: {
-                    friends: {
-                        friendId: id,
-                        status: true,
-                    },
-                },
-            }
-        );
+    // Add the corresponding relation to the current user's document
+    const updateCurrentUserRelation = await userModel.updateOne(
+      { _id: currentUserId },
+      {
+        $push: {
+          friends: {
+            friendId: id,
+            status: true,
+          },
+        },
+      }
+    );
 
-        const deleteUserNotification = await notificationModel.deleteOne({userId:currentUserId});
-        const sendRequestAcceptNotification = await notificationModel.create({userId:id,notification: {notificationType:"accept-request"}, senderId:currentUserId})
-        if (updatedUser && updateCurrentUserRelation) {
-            res.status(200).json({ message: "Friend request accepted successfully" });
-        } else {
-            res.status(404).json({ error: "User or friend request not found" });
-        }
-    } catch (error) {
-        console.error("Error accepting friend request:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+    const deleteUserNotification = await notificationModel.deleteOne({
+      userId: currentUserId,
+    });
+    const sendRequestAcceptNotification = await notificationModel.create({
+      userId: id,
+      notification: { notificationType: "accept-request" },
+      senderId: currentUserId,
+    });
+    if (updatedUser && updateCurrentUserRelation) {
+      res.status(200).json({ message: "Friend request accepted successfully" });
+    } else {
+      res.status(404).json({ error: "User or friend request not found" });
     }
+  } catch (error) {
+    console.error("Error accepting friend request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-
-
