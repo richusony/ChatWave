cf1f269b172d43ecccd796dba66d68c6f762d393
Richu Sony <sonyrichu4@gmail.com>
2024-04-10 01:37:52 +0530

    feat: Add user typing status

--- socket/socket.js
+++ socket/socket.js
@@ -7,14 +7,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [`https://richu-chatwave.vercel.app`], // https://richu-chatwave.vercel.app
+    origin: [`http://localhost:5173`], // https://richu-chatwave.vercel.app
     methods: ["GET", "POST"],
   },
 });
 
 export const getReceiverSocketId = (receiverId) => {
   return userSocketMap[receiverId];
-}
+};
 
 const userSocketMap = {};
 
@@ -22,15 +22,32 @@ io.on("connection", (socket) => {
   console.log("a user connect", socket.id);
 
   const userId = socket.handshake.query.userId;
-  console.log("handshakeId ",userId)
+  console.log("handshakeId ", userId);
   if (userId !== "undefined") userSocketMap[userId] = socket.id;
 
   // emit is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("userTyping", (data) => {
+  const receiverSocketId = getReceiverSocketId(data.receiverId);
+  if (receiverSocketId) {
+    io.to(receiverSocketId).emit("userIsTyping", data.message);
+  }
+});
+
+socket.on("userStoppedTyping", (data) => {
+  const receiverSocketId = getReceiverSocketId(data.receiverId);
+  if (receiverSocketId) {
+    io.to(receiverSocketId).emit("userStoppedTyping");
+  }
+});
+
+
+
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
-    delete userSocketMap[userId]
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
